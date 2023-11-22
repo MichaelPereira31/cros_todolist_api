aa67b34d5ac0abed171a6de6c44fa27fdad082e8
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-11-22 19:39:11 -0300

    feat: add create user

---
+++ src/modules/user/useCases/create/CreateUserUseCase.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from 'tsyringe';
+import { encrypt } from 'util/encrypt';
+
+import { ICreateUserDTO } from '@modules/user/dtos/ICreateUserDTO';
+import { IUserRepository } from '@modules/user/repositories/IUserRepository';
+import { User } from '@prisma/client';
+import { AppError } from '@shared/infra/errors/AppError';
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject('UserRepository')
+    private readonly userRepository: IUserRepository,
+  ) {}
+
+  async execute({ email, password, name }: ICreateUserDTO): Promise<User> {
+    const userAlreadyExists = await this.userRepository.findByEmail(email);
+
+    if (userAlreadyExists) throw new AppError('USER_ALREADY_EXISTS');
+
+    const user = await this.userRepository.create({
+      email,
+      password: encrypt({ password }),
+      name,
+    });
+
+    return user;
+  }
+}
---
+++ src/util/encrypt.ts
@@ -0,0 +1,21 @@
+import { createCipheriv, randomBytes } from 'crypto';
+
+export type IEncryptDTO = {
+  ivEncrypt?: string;
+  password: string;
+};
+
+export function encrypt({ ivEncrypt, password }: IEncryptDTO): string {
+  const iv = ivEncrypt ? Buffer.from(ivEncrypt) : randomBytes(16);
+
+  const cipher = createCipheriv(
+    'aes-256-cbc',
+    Buffer.from(process.env.ENCRYPT_KEY as string),
+    iv,
+  );
+
+  const encrypting = cipher.update(password);
+
+  const encrypted = Buffer.concat([encrypting, cipher.final()]);
+  return `${iv.toString('hex')}-${encrypted.toString('hex')}`;
+}
