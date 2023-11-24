e5c206546261e4a8699f4b45cc1510e925a9f439
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-11-24 01:59:02 -0300

    refactor: refactoring create task

--- src/modules/task/useCases/create/CreateTaskUseCase.ts
+++ src/modules/task/useCases/create/CreateTaskUseCase.ts
@@ -19,13 +19,6 @@ export class CreateTaskUseCase {
     userId,
     parentId,
   }: ICreateTaskDTO): Promise<Task> {
-    const taskAlreadyExists = await this.taskRepository.find({
-      description,
-      title,
-    });
-
-    if (taskAlreadyExists) throw new AppError('TASK_ALREADY_EXISTS');
-
     if (parentId) {
       await this.taskRepository.findById(parentId).catch(() => {
         throw new AppError('PARENT_TASK_ALREADY_EXISTS');
@@ -37,12 +30,7 @@ export class CreateTaskUseCase {
       title,
       status,
       userId,
-    });
-
-    await this.taskRepository.update({
-      id: parentId,
-      parentId: task.id,
-      userId,
+      parentId,
     });
 
     return task;
