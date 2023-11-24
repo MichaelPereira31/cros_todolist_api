ab62922bf1278795eda17b4f8b293a1f7dc24798
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-11-23 23:37:49 -0300

    docs: add codumentaion

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,9 @@
 import 'express-async-errors';
 import 'reflect-metadata';
-
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
+import swaggerDocs from '../../../docs/swagger.json';
 import { getErrors } from '../errors/getErrors';
 import { logger } from '../providers/logger/implementations/LoggerProvider';
 import responseFormatter from './middleware/responseFormatter';
@@ -16,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(responseFormatter);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(routes);
 app.use(getErrors);
 
